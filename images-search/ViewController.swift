d0fde7c65aa4149922f5fbf20df39337a1fd993a
Anisovich_Maria <anisovich.masha@gmail.com>
2024-11-06 15:19:25 +0100

    Initial Commit

---
+++ images-search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  images-search
+//
+//  Created by Мария Анисович on 06.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
